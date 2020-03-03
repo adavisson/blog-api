800149463a14b752a6b31f3170ce6687649fcf45
Andrew Davisson <akdavisson4@gmail.com>
2020-03-03 14:23:11 -0700

    created blog seed file

---
+++ src/persistence/seeders/20200303212111-blog-data.js
@@ -0,0 +1,14 @@
+'use strict';
+const data = require('../seedData/testData');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+
+   return queryInterface.bulkInsert('Blogs', data);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    
+    return queryInterface.bulkDelete('Blogs', null, {});
+  }
+};
