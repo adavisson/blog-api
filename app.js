880beff4ff6395ef72e3574d827163f961e12120
Andrew Davisson <akdavisson4@gmail.com>
2020-03-03 22:07:16 -0700

    added cors to work locally

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const blogRouter = require('./src/routes/blog');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express()
 const port = 3001
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
